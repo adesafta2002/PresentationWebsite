2091ca144e3f6233b94ac4711298ebb6f44476a7
adesafta2002 <ade.safta@yahoo.com>
2020-11-19 12:04:12 +0200

    Body update

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -18,11 +18,11 @@ const Body = ({theme}) =>{
                     <p> &nbsp; &nbsp;  &nbsp; Hello everyone and welcome to my presentation Website! I am  an 18 years old Romanian, and a first year 
                         student at the Faculty of Mathematics and Computer Science of the Univeristy of Bucharest.
                         I am very passionate about what I do, and I put a lot of time and effort into always learning new things as fast  and 
-                        efficient as possible. Getting into C++ two years ago wasn't easy for me, but after a few months and houndreds of problems it became
-                        the thing i loved doing. After a while, Web Development caught my eye, and I started learning HTML, CSS, JS, and recently I
+                        efficient as possible. Getting into C++ two years ago wasn't easy for me, but after a few months and hundreds of problems it became
+                        the thing i love doing. After a while, Web Development caught my eye, and I started learning HTML, CSS, JS, and recently I
                          made the jump to React, which is very enjoyable.<br>
                          </br>
-                         &nbsp; &nbsp;  &nbsp; Beside this, I have other things I'm passionate about, like Hiking, long  walks on the beach, exploring new cities, and other 
+                         &nbsp; &nbsp;  &nbsp; Beside this, I am passionate about other things, like Hiking, long  walks on the beach, exploring new cities, and other 
                          activities related to this.
 
 
